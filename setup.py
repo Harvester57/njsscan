5b0edff72c430dce3607d9c4b40d9bb0ed79c58a
Harvester57 <florian.stosse@gmail.com>
2025-08-06 10:28:49 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     install_requires=[
         'colorama',
         'libsast',
-        'semgrep=',
+        'semgrep',
         'sarif-om',
         'jschema-to-python',
         'tabulate',
